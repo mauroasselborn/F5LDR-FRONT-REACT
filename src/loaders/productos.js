a5f50a06dd86e666ed6e1067daeb1377c6394beb
Mauro <mauasselborn@gmail.com>
2023-02-04 14:46:41 -0300

    Minimos retoques para trabajar desde la network

--- src/loaders/productos.js
+++ src/loaders/productos.js
@@ -1,5 +1,7 @@
 export const loaderBebidas = async () => {
-    const res = await fetch('http://localhost:3000/products')
+    // const res = await fetch('http://localhost:3000/products') para trabajar desde local
+    const res = await fetch('http://192.168.2.166:3000/products') //Para laburar desde la network
+
     const productos = await res.json()
 
     const bebidas = productos.filter((producto) => producto.Categoria === 'Bebidas')
@@ -7,7 +9,8 @@ export const loaderBebidas = async () => {
     return bebidas
 }
 export const loaderComidas = async () => {
-    const res = await fetch('http://localhost:3000/products')
+    // const res = await fetch('http://localhost:3000/products') para trabajar desde local
+    const res = await fetch('http://192.168.2.166:3000/products') //Para laburar desde la network
     const productos = await res.json()
 
     const comidas = productos.filter((producto) => producto.Categoria === 'Comidas')
@@ -15,7 +18,8 @@ export const loaderComidas = async () => {
     return comidas
 }
 export const loaderSnacks = async () => {
-    const res = await fetch('http://localhost:3000/products')
+    // const res = await fetch('http://localhost:3000/products') para trabajar desde local
+    const res = await fetch('http://192.168.2.166:3000/products') //Para laburar desde la network
     const productos = await res.json()
 
     const snacks = productos.filter((producto) => producto.Categoria === 'Snacks')
