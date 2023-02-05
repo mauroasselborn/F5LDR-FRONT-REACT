35dddb28bad3c1bc1e263d62fc89678255d616da
Mauro <mauasselborn@gmail.com>
2023-02-05 17:57:30 -0300

    bug fixed

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,8 @@
 export const Form = () => {
     const cargarProducto = async (e) => {
         e.preventDefault()
-        const url = 'http://localhost:3000/product'
+        // const url = 'http://localhost:3000/product'
+        const url = 'http://192.168.2.166:3000/product'
         const data = {
             description: 'Prueba de POST',
             size: 1000,
@@ -9,7 +10,7 @@ export const Form = () => {
             stock: 10,
             stock_min: 50,
             brand_id: 2,
-            category_id: 3,
+            category_id: 1,
         }
 
         await fetch(url, {
